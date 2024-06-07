f293989b6bbdeb1bc74df6019f74a1671e2ac4d6
ektaSahani94 <87010536+ektaSahani94@users.noreply.github.com>
2024-06-07 14:38:14 +0530

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Home = () => {
   return (
     <div className="container">
-      <h1>Home Page for TW</h1>
+      <h1>Home Page for Thoughtworks</h1>
     </div>
   );
 };
