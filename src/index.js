6580b7e3def8a6a52a2cec8c7c5246553a27d57d
Ravinder Mahajan <88770719+ravinder-tw@users.noreply.github.com>
2024-06-08 22:13:57 +0530

    updated cf template

--- src/index.js
+++ src/index.js
@@ -1,20 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';
+import * as React from "react";
+import { createRoot } from "react-dom/client";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Home from "./pages/Home";
+import Products from "./pages/Products";
+import './App.css';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-   <BrowserRouter>
-    <App />
-  </BrowserRouter>
-  </React.StrictMode>
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home/>,
+  },
+  {
+    path: "products",
+    element: <Products/>
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
+  <div className='App-header'>
+    <RouterProvider router={router} />
+  </div>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
+import "./../App.css";
+import {
+  Link,
+} from "react-router-dom";
+
 
 const Home = () => {
   return (
-    <div className="container">
-      <h1>Home Page for Thoughtworks.</h1>
-    </div>
+      <div className='container'>
+        <h1>Home Page for ci-cd</h1>
+        <Link to='products'>Products</Link>
+      </div>
   );
 };
 
