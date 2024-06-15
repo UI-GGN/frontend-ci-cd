37b02c149a2f9c9c7c86ec0bb15161631304e419
Ekta <ekta.sahani@thoughtworks.com>
2024-06-15 12:32:11 +0530

    Change Home page text

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,7 +5,7 @@ const HomePage = () => {
   return (
     <>
       <div className="home-page">
-        <h1>Welcome to Our Store</h1>
+        <h1>Welcome to Our Frontend Store</h1>
         <p>Browse our collection of amazing products!</p>
         <Link to="/products">Go to Products page!</Link>
       </div>
